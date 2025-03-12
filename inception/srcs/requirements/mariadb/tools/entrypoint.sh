ac4e8b1420a763fd854746a60153ddd1c6215236
sparklebutt <arandomsanti@gmail.com>
2025-03-12 14:55:33 +0200

    fix: mysql start

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -9,19 +9,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-#chown -R
+chown -R mysql:mysql /var/lib/mysql /run/mysqld /var/log/mysql
 # Ensure directory permissions
-if ! chown -R mysql:mysql /var/lib/mysql /run/mysqld /var/log/mysql; then
-  echo "Error: Failed to set directory permissions"
-  exit 1
-fi
-
-# Start the MariaDB service
-#if ! mysqld_safe &; then
-#  echo "Error: Failed to start MariaDB service"
+#if ! chown -R mysql:mysql /var/lib/mysql /run/mysqld /var/log/mysql; then
+#  echo "Error: Failed to set directory permissions"
 #  exit 1
 #fi
 
+mysqld_safe &
 # Wait for MariaDB to be ready
 until mysqladmin ping --silent; do
   echo "Waiting for MariaDB to be ready..."
